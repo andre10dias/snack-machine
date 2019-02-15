1612d6ddbdbd22704354d6b305bc277dbf99f895
Carlos Dias <andre10dias@ymail.com>
2019-02-14 21:52:59 -0300

    Produto e associação muitos para muitos.

--- src/main/java/com/arquitetura/orientada/servicos/SnackMachineApplication.java
+++ src/main/java/com/arquitetura/orientada/servicos/SnackMachineApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.arquitetura.orientada.servicos.domain.Maquina;
+import com.arquitetura.orientada.servicos.domain.Produto;
 import com.arquitetura.orientada.servicos.repositories.MaquinaRepository;
+import com.arquitetura.orientada.servicos.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class SnackMachineApplication implements CommandLineRunner {
 
 	@Autowired
 	private MaquinaRepository maquinaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SnackMachineApplication.class, args);
@@ -28,7 +33,26 @@ public class SnackMachineApplication implements CommandLineRunner {
 		Maquina maq4 = new Maquina(null, "Maquina 4");
 		Maquina maq5 = new Maquina(null, "Maquina 5");
 		
+		Produto p1 = new Produto(null, "Sanduíche Natural", 6.00);
+		Produto p2 = new Produto(null, "Coockies", 4.00);
+		Produto p3 = new Produto(null, "Água Mineral com Gás", 2.50);
+		Produto p4 = new Produto(null, "Água Mineral", 2.00);
+		Produto p5 = new Produto(null, "Barra de Cereal", 1.50);
+		
+		maq1.getProdutos().addAll(Arrays.asList(p1, p1, p1, p1, p1, p2, p2, p3, p4, p4, p5, p5, p5));
+		maq2.getProdutos().addAll(Arrays.asList(p1, p2, p2, p3, p3, p3, p3, p3, p5, p5));
+		maq3.getProdutos().addAll(Arrays.asList(p2, p2, p2, p2, p3, p4, p5));
+		maq4.getProdutos().addAll(Arrays.asList(p3, p3, p3, p3, p3, p3, p3, p3, p4, p4, p4, p4, p4));
+		maq5.getProdutos().addAll(Arrays.asList(p1, p1, p1, p1, p1));
+		
+		p1.getMaquinas().addAll(Arrays.asList(maq1, maq2, maq5));
+		p2.getMaquinas().addAll(Arrays.asList(maq1, maq2, maq3));
+		p3.getMaquinas().addAll(Arrays.asList(maq1, maq2, maq3, maq4));
+		p4.getMaquinas().addAll(Arrays.asList(maq1, maq3, maq4));
+		p5.getMaquinas().addAll(Arrays.asList(maq1, maq2, maq3));
+		
 		maquinaRepository.saveAll(Arrays.asList(maq1, maq2, maq3, maq4, maq5));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 	}
 
 }
--- src/main/java/com/arquitetura/orientada/servicos/domain/Produto.java
+++ src/main/java/com/arquitetura/orientada/servicos/domain/Produto.java
@@ -8,26 +8,35 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
 @Entity
-public class Maquina implements Serializable {
+public class Produto implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+	private Double preco;
 	
-	@ManyToMany(mappedBy="maquinas")
-	private List<Produto> produtos = new ArrayList<>();
-	
-	public Maquina() {}
+	@ManyToMany
+	@JoinTable(name = "PRODUTO_MAQUINA",
+		joinColumns = @JoinColumn(name = "produto_id"),
+		inverseJoinColumns = @JoinColumn(name = "maquina_id")
+	)
+	private List<Maquina> maquinas = new ArrayList<>();
 
-	public Maquina(Integer id, String nome) {
+	public Produto() {
+	}
+
+	public Produto(Integer id, String nome, Double preco) {
 		super();
 		this.id = id;
 		this.nome = nome;
+		this.preco = preco;
 	}
 
 	public Integer getId() {
@@ -46,14 +55,22 @@ public class Maquina implements Serializable {
 		this.nome = nome;
 	}
 
-	public List<Produto> getProdutos() {
-		return produtos;
+	public Double getPreco() {
+		return preco;
 	}
 
-	public void setProdutos(List<Produto> produtos) {
-		this.produtos = produtos;
+	public void setPreco(Double preco) {
+		this.preco = preco;
 	}
 
+	public List<Maquina> getMaquinas() {
+		return maquinas;
+	}
+
+	public void setMaquinas(List<Maquina> maquinas) {
+		this.maquinas = maquinas;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -70,7 +87,7 @@ public class Maquina implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Maquina other = (Maquina) obj;
+		Produto other = (Produto) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -78,6 +95,5 @@ public class Maquina implements Serializable {
 			return false;
 		return true;
 	}
-	
-	
+
 }
